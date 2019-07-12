7be3218695dc070950d39b02d805b0d0f030268d
Jonathan Ross <jonross@alum.mit.edu>
2019-07-12 07:12:39 -0400

    Add assertThrows

--- src/test/java/com/github/jonross/stuff4j/Utils.java
+++ src/test/java/com/github/jonross/stuff4j/Utils.java
@@ -5,6 +5,12 @@ import java.io.FilterReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
+import java.util.regex.Pattern;
+
+import com.github.jonross.stuff4j.function.Throwing;
+
+import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.fail;
 
 class Utils {
 
@@ -36,4 +42,16 @@ class Utils {
         }
     }
 
+    static void assertThrows(Class<? extends Exception> klass, String message, Throwing.Runnable r) {
+        try {
+            r.run();
+            fail("Expected exception of type " + klass);
+        }
+        catch (Exception e) {
+            assertTrue(klass.isAssignableFrom(e.getClass()),
+                    "Expected an instance of " + klass + " but got " + e);
+            assertTrue(Pattern.compile(message).matcher(e.getMessage()).matches(),
+                    "Expected message to match '" + message + "' but got " + e.getMessage());
+        }
+    }
 }
